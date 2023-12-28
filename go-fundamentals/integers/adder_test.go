f6bd16ae5e10c61f3eb37ee44e4d65e512690c3c
arturbaccarin <arturbaccarin@github.com>
2023-12-28 19:15:19 -0300

    feat: add integer part

---
+++ go-fundamentals/integers/adder_test.go
@@ -0,0 +1,33 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	t.Run("Add 2 and 2", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
+
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
+
+	t.Run("Add 3 and 5", func(t *testing.T) {
+		sum := Add(3, 5)
+		expected := 8
+
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
+
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
