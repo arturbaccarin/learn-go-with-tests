be6a04eedb2abfb4a7472c225f2faf49dc21b15c
arturbaccarin <baccarin.artur@gmail.com>
2023-12-30 11:05:48 -0300

    feat: add triangle in tests

--- go-fundamentals/struct-method-interface/struct-method-interface_test.go
+++ go-fundamentals/struct-method-interface/struct-method-interface_test.go
@@ -45,6 +45,7 @@ func TestAreaTableDrivenTests(t *testing.T) {
 	}{
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
