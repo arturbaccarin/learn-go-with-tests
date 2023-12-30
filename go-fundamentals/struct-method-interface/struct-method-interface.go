e807403ee8f0c151b0a79a493e75e03cdd5757d9
arturbaccarin <arturbaccarin@github.com>
2023-12-30 11:04:17 -0300

    feat: part one of struct method and interface

---
+++ go-fundamentals/struct-method-interface/struct-method-interface.go
@@ -0,0 +1,33 @@
+package structmethodinterface
+
+import "math"
+
+// They allow you to make functions that can be used with different types
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
---
+++ go-fundamentals/struct-method-interface/struct-method-interface_test.go
@@ -0,0 +1,56 @@
+package structmethodinterface
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	// By declaring an interface, the helper is decoupled from the concrete types and only has the method it needs to do its job.
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			// Use of g will print a more precise decimal number in the error message
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
+
+// Table driven tests are useful when you want to build a list of test cases that can be tested in the same manner.
+func TestAreaTableDrivenTests(t *testing.T) {
+
+	// anonymous struct
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+}
