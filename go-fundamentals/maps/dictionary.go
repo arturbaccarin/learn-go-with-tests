4df44e1ca7dbe94fdffc89c088f54d0e500f24e8
arturbaccarin <baccarin.artur@gmail.com>
2024-01-02 06:45:06 -0300

    finish maps part

--- go-fundamentals/maps/dictionary.go
+++ go-fundamentals/maps/dictionary.go
@@ -1,13 +1,21 @@
 package maps
 
-import "errors"
-
 // The key type is special.
 // It can only be a comparable type because without the ability to tell if 2 keys are equal.
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -16,4 +24,41 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+// An interesting property of maps is that you can modify them without passing as an address to it (e.g &myMap)
+func (d Dictionary) Add(word, definition string) error {
+	_, ok := d[word]
+	if ok {
+		return ErrWordExists
+	}
+
+	d[word] = definition
+	return nil
+}
+
+/*
+A gotcha with maps is that they can be a nil value.
+A nil map behaves like an empty map when reading,
+but attempts to write to a nil map will cause a runtime panic.
+*/
+
+// Having specific errors gives you more information about what went wrong.
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
