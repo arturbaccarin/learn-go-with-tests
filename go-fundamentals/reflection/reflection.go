cf1758c212ec489e6abbec42089c9d34765579d8
arturbaccarin <baccarin.artur@gmail.com>
2024-01-08 08:55:15 -0300

    start reflection chapter

---
+++ go-fundamentals/reflection/reflection.go
@@ -0,0 +1,31 @@
+package reflection
+
+import "reflect"
+
+// Reflection in computing is the ability of a program to examine its own structure, particularly through types
+
+// write a function walk(x interface{}, fn func(string)) which takes a struct x and calls fn for all strings fields found inside. difficulty level: recursively.
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
+}
