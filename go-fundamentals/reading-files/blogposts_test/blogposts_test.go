156eaac46220cac538c03c1f874264f26e6ac42f
arturbaccarin <baccarin.artur@gmail.com>
2024-01-15 08:09:40 -0300

    start readfile chapter

---
+++ go-fundamentals/reading-files/blogposts_test/blogposts_test.go
@@ -0,0 +1,42 @@
+package blogposts_test
+
+import (
+	"errors"
+	"io/fs"
+	"reflect"
+	"testing"
+	"testing/fstest"
+
+	"github.com/arturbaccarin/learn-go-with-tests/go-fundamentals/reading-files/blogposts"
+)
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
