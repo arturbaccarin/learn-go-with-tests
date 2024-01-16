12c64e6d3509b8e0dfc6c8a5e8b7de21dbc88588
arturbaccarin <baccarin.artur@gmail.com>
2024-01-16 08:25:52 -0300

    finish reading files chapter and delete math chapter

--- go-fundamentals/reading-files/blogposts/blogposts.go
+++ go-fundamentals/reading-files/blogposts/blogposts.go
@@ -1,11 +1,21 @@
 package blogposts
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
 	"io/fs"
+	"strings"
 )
 
+//when TDD is practiced well we take a consumer-driven approach
+
 type Post struct {
-	Title string
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
 }
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
@@ -14,8 +24,60 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err //todo: needs clarification, should we totally fail if one file fails? or just ignore?
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
+
+// Does newPost have to be coupled to an fs.File ? Do we use all the methods and data from this type? What do we really need?
+func newPost(postFile io.Reader) (Post, error) {
+	// Scanner provides a convenient interface for reading data such as a file of newline-delimited lines of text.
+	scanner := bufio.NewScanner(postFile)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
+	}
+
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
+
+	buf := bytes.Buffer{}
+
+	/*
+		scanner.Scan() returns a bool which indicates whether there's more data to scan,
+		so we can use that with a for loop to keep reading through the data until the end.
+	*/
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
 }
--- go-fundamentals/reading-files/blogposts_test/blogposts_test.go
+++ go-fundamentals/reading-files/blogposts_test/blogposts_test.go
@@ -18,9 +18,25 @@ func (s StubFailingFS) Open(name string) (fs.File, error) {
 }
 
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
@@ -33,9 +49,17 @@ func TestNewBlogPosts(t *testing.T) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
 	}
 
-	got := posts[0]
-	want := blogposts.Post{Title: "Post 1"}
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+}
 
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %+v, want %+v", got, want)
 	}
