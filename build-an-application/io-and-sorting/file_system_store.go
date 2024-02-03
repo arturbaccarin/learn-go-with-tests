b541238469c1f8a13984ca53295f6549bcfbea0c
arturbaccarin <baccarin.artur@gmail.com>
2024-02-03 10:02:48 -0300

    doing io and sorting chapter

---
+++ build-an-application/io-and-sorting/file_system_store.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+)
+
+type FileSystemPlayerStore struct {
+	database io.ReadSeeker
+}
+
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	f.database.Seek(0, 0)
+	league, _ := NewLeague(f.database)
+	return league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	return 0
+}
